86341ffd3e361c05b38e6fd8d2fd40a294f280ee
Kshitiz Ranjan <kshitizranjan154@gmail.com>
2020-09-14 12:25:52 +0530

    A3Q7 is added.

---
+++ A3Q7.java
@@ -0,0 +1,45 @@
+package ASSIGNMENT_3;
+
+import java.util.Scanner;
+
+public class A3Q7 {
+
+	public static void main(String[] args)
+	{
+		Scanner sc= new Scanner(System.in);
+		double x,y;
+		System.out.println("Enter the value of x");
+        x=sc.nextDouble();
+        System.out.println("Enter the value of y");
+        y=sc.nextDouble();
+        if(x>0 && y>0)
+        {
+        	System.out.println("( "+x+", "+y+" )lies in 1st Quadrant");
+        }
+        else if(x<0 && y>0)
+        {
+        	System.out.println("( "+x+", "+y+" ) lies in 2nd Quadrant");
+        }
+        else if(x<0 && y<0)
+        {
+        	System.out.println("( "+x+", "+y+" ) lies in 3rd Quadrant");
+        }
+        else if (x>0 && y<0)
+        {
+        	System.out.println("( "+x+", "+y+" ) lies in 4th Quadrant");
+        }
+        else if (x==0 && y==0)
+        {
+        	System.out.println("( "+x+", "+y+" ) lies on origin");
+        }
+        else if (x==0)
+        {
+        	System.out.println("( "+x+", "+y+" ) lies on Y-axis");
+        }
+        else if (y==0)
+        {
+        	System.out.println("( "+x+", "+y+") lies on X-axis");
+        }
+	}
+
+}
