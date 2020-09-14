87fdcae0b7f558c56eb1264807351b1bdfd56417
Kshitiz Ranjan <kshitizranjan154@gmail.com>
2020-09-14 12:24:41 +0530

    Initial Commit

---
+++ q7.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class q67 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int x;
+		x=n+n*n+n*n*n;
+		System.out.println(x);
+
+	}
+
+}
